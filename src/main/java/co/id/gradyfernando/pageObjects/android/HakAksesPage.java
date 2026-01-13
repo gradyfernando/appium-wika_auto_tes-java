bd612373c65f73de00b17a5cc1f1a74961dc0d41
Grady Fernando <gradyfernando92@gmail.com>
2026-01-13 21:50:06 +0700

    add hak akses page object

--- src/main/java/co/id/gradyfernando/pageObjects/android/HakAksesPage.java
+++ src/main/java/co/id/gradyfernando/pageObjects/android/HakAksesPage.java
@@ -3,9 +3,13 @@ package co.id.gradyfernando.pageObjects.android;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
+import com.google.common.collect.ImmutableMap;
+
+import co.id.gradyfernando.model.Akses;
 import co.id.gradyfernando.utils.AndroidActions;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
@@ -18,10 +22,20 @@ public class HakAksesPage extends AndroidActions {
 	@AndroidFindBy(id = "co.id.integra.weoffice:id/btnLogout")
 	private WebElement logoutButton;
 
+    private Akses akses;
+
     public HakAksesPage(AndroidDriver driver) {
         super(driver);
+        setActivity();
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
+
+	public void setActivity() {
+		String activityName = "co.id.integra.weoffice/co.id.integra.weoffice.view.activity.HakAksesActivity";
+		((JavascriptExecutor) driver).executeScript("mobile: startActivity", ImmutableMap.of(
+			"intent", activityName
+		));
+	}
 
     public void setRole(String role) {
         List<WebElement> roleElements = driver.findElements(By.id("co.id.integra.weoffice:id/tvNama"));
@@ -30,10 +44,11 @@ public class HakAksesPage extends AndroidActions {
         for (int i=0; i<roleSize; i++) {
             String roleName = roleElements.get(i).getText();
             
-            if (roleName.toLowerCase().contains(role)) {
+            if (roleName.toLowerCase().contains(role.toLowerCase())) {
                 roleElements.get(i).click();
             }
         }
+        
     }
 
     

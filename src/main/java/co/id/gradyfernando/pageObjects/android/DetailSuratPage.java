9013351025ed00560e93a9bd2f12352a304d8a6d
Grady Fernando <gradyfernando92@gmail.com>
2026-01-14 16:36:26 +0700

    feat: add capability to open page by deep link web url

--- src/main/java/co/id/gradyfernando/pageObjects/android/DetailSuratPage.java
+++ src/main/java/co/id/gradyfernando/pageObjects/android/DetailSuratPage.java
@@ -1,5 +1,8 @@
 package co.id.gradyfernando.pageObjects.android;
 
+import java.util.List;
+import java.util.Map;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
@@ -10,20 +13,36 @@ import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
 public class DetailSuratPage extends AndroidActions {
 
+	@AndroidFindBy(id = "co.id.integra.weoffice:id/tvNoSurat")
+	private WebElement noSuratLabel;
 	@AndroidFindBy(id = "co.id.integra.weoffice:id/btnKirimDisposisi")
 	private WebElement disposisiButton;
 	@AndroidFindBy(id = "co.id.integra.weoffice:id/btnKirimInformasi")
 	private WebElement informasiButton;
 
-    public DetailSuratPage(AndroidDriver driver, String suratId) {
+    public DetailSuratPage(AndroidDriver driver) {
         super(driver);
-        setActivity(suratId);
-
 		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
     public void setActivity(String suratId) {
         driver.get("weoffice://surat/detail?id=" + suratId);
+    }
+
+    public void openPageByDeepLinkWebUrl(String url) {
+        driver.executeScript("mobile: shell", Map.of(
+            "command", "am",
+            "args", List.of(
+                "start",
+                "-a", "android.intent.action.VIEW",
+                "-d", url,
+                "co.id.integra.weoffice"
+            )
+        ));
+    }
+
+    public String getNomorSurat() {
+        return noSuratLabel.getText();
     }
 
     public void clickDisposisikan() {

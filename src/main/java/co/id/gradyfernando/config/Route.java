7b66635cefa4627c0a7c0512472225adcee58bc1
Grady Fernando <gradyfernando92@gmail.com>
2026-01-14 16:31:30 +0700

    add endpoint getlistsurat & getlistdisposisi

--- src/main/java/co/id/gradyfernando/config/Route.java
+++ src/main/java/co/id/gradyfernando/config/Route.java
@@ -3,4 +3,9 @@ package co.id.gradyfernando.config;
 public class Route {
 	public static final String LOGIN = "login";
 	public static final String USER_GET_AKSES = "getakses";
+	
+	public static final String GET_LIST_SURAT_MASUK = "getlistsuratmasuk";
+	public static final String GET_LIST_SURAT_KELUAR = "getlistsuratkeluar";
+	public static final String GET_LIST_DISPOSISI_MASUK = "getlistdisposisimasuk";
+	public static final String GET_LIST_DISPOSISI_KELUAR = "getlistdisposisikeluar";
 }

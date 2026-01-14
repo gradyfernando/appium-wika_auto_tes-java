1ae44a912bd275d57090fd15fae8896c2be54096
Grady Fernando <gradyfernando92@gmail.com>
2026-01-14 16:31:05 +0700

    fix: set base url & base api url

--- src/main/java/co/id/gradyfernando/config/ApiConfig.java
+++ src/main/java/co/id/gradyfernando/config/ApiConfig.java
@@ -1,6 +1,8 @@
 package co.id.gradyfernando.config;
 
 public class ApiConfig {
-	public static final String BASE_URL = "https://woffice-demo.integrasolusi.com/index.php/api/";
+	public static final String BASE_URL = "https://woffice-demo.integrasolusi.com";
+
+	public static final String BASE_API_URL = BASE_URL + "/index.php/api/";
 }
 

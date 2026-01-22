f8356b0bbf74466dffd4dca3c29ad09997332aaf
Grady Fernando <gradyfernando92@gmail.com>
2026-01-22 14:54:27 +0700

    fix: init element

--- src/main/java/co/id/gradyfernando/pageObjects/android/DaftarDraftPage.java
+++ src/main/java/co/id/gradyfernando/pageObjects/android/DaftarDraftPage.java
@@ -1,13 +1,16 @@
 package co.id.gradyfernando.pageObjects.android;
 
+import org.openqa.selenium.support.PageFactory;
+
 import co.id.gradyfernando.utils.AndroidActions;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
 public class DaftarDraftPage extends AndroidActions {
     
     public DaftarDraftPage(AndroidDriver driver) {
         super(driver);
-        
+		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
     }
 
 }
